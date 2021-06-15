68fefdd5ecc8307614e719aa4b607f1cc3ba3e80
samuelarbibe <samuel.arbibe@gmail.com>
2021-06-15 23:17:22 +0300

    Fix morgan logging in prod

--- src/middlewares/morganMiddleware.js
+++ src/middlewares/morganMiddleware.js
@@ -8,7 +8,7 @@ const stream = {
 
 const skip = () => {
   const env = process.env.NODE_ENV || "development"
-  return env !== "development"
+  return env === "test"
 }
 
 const morganMiddleware = morgan(
