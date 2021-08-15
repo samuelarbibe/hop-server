7886abaf4d8ddacb8e8305d172ad95828a81a29b
samuelarbibe <samuel.arbibe@gmail.com>
2021-08-15 22:04:17 +0300

    Test

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -78,6 +78,10 @@ const cancelOrder = async (req, res, next) => {
 const updateOrderStatus = async (req, res, next) => {
   try {
     const transactionDetails = req.body
+    Object.keys(transactionDetails).forEach((key) => {
+      logger.info(key + ': ' + transactionDetails[key])
+    })
+
     const { cField1: orderId, cField2: cartId } = transactionDetails.customFields
 
     await approveTransaction(transactionDetails)
